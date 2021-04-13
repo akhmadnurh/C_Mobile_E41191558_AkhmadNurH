d187ef83f19a8c5d811e44424c30ff2ebc652c7f
monokuro49 <07akhmadnur@gmail.com>
2021-04-13 21:02:02 +0700

    Tugas Minggu 6 - Intent

---
+++ Minggu 6/Intent/app/src/main/java/com/akhmadnur/intent/MoveWithDataActivity.java
@@ -0,0 +1,23 @@
+package com.akhmadnur.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MoveWithDataActivity extends AppCompatActivity {
+
+    private TextView tvdataintent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_data);
+
+        tvdataintent = findViewById(R.id.tvdataintent);
+
+        String nama = getIntent().getStringExtra("extra_name");
+        int age = getIntent().getIntExtra("extra_age", 0);
+        String text = "Nama: "+nama+", usia: "+age;
+        tvdataintent.setText(text);
+    }
+}
