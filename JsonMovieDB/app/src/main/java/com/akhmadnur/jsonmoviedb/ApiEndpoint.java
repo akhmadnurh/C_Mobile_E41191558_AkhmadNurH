f1392310ed6b65f81ec694989ca5b8df021dc829
Akhmad Nur Hidayatulloh <e41191558@student.polije.ac.id>
2021-06-24 10:05:02 +0700

    MovieDB

---
+++ JsonMovieDB/app/src/main/java/com/akhmadnur/jsonmoviedb/ApiEndpoint.java
@@ -0,0 +1,13 @@
+package com.akhmadnur.jsonmoviedb;
+
+public class ApiEndpoint {
+
+    public static String BASEURL = "https://api.themoviedb.org/3/";
+    public static String APIKEY = "api_key=04b659d1ac7bab377d45ab949a657542";
+    public static String LANGUAGE = "&language=en-US";
+    public static String SEARCH_MOVIE = "search/movie?";
+    public static String QUERY = "&query=";
+    public static String MOVIE_PLAYNOW = "movie/now_playing?";
+    public static String MOVIE_POPULAR = "discover/movie?";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w780/";
+}
--- JsonMovieDB/app/src/main/java/com/akhmadnur/jsonmoviedb/MainActivity.java
+++ JsonMovieDB/app/src/main/java/com/akhmadnur/jsonmoviedb/MainActivity.java
@@ -2,7 +2,11 @@ package com.akhmadnur.jsonmoviedb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView showJson = (ImageView) findViewById(R.id.json);
+        ImageView showJsonAPI = (ImageView) findViewById(R.id.json_api);
+        ImageView showMovieDB = (ImageView) findViewById(R.id.moviedb);
+
+        showJson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, JsonParseActivity.class);
+                startActivity(intent);
+            }
+        });
+        showJsonAPI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, JsonAPIActivity.class);
+                startActivity(intent);
+            }
+        });
+        showMovieDB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MovieDBActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
